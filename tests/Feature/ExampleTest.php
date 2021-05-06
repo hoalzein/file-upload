cfa728a3b2b2a75982b3b297f2f1a26612984f89
hoalzein <hashim.omar.zein@hotmail.com>
2021-05-07 01:08:18 +0200

    Upload tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,15 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+class ExampleTest extends TestCase {
 
-        $response->assertStatus(200);
+    public function testFileUpload() {
+        Storage::fake('app/public/files/');
+        Storage::disk('app/public/files/')->assertExists('file.pdf');
+        Storage::disk('app/public/files/')->assertMissing('missing.docx');
     }
+
 }
