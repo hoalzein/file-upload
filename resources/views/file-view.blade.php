3e4b94f1a1df46c3f55c3c513d9f65f34e106944
hoalzein <hashim.omar.zein@hotmail.com>
2021-05-06 19:26:43 +0200

    File overview implementation

---
+++ resources/views/file-view.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+        <title>Simple File Upload</title>
+        <style>
+            .container {
+                max-width: 500px;
+            }
+            dl, ol, ul {
+                margin: 0;
+                padding: 0;
+                list-style: none;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container mt-5">
+            {!! $html !!}
+        </div>
+    </body>
+</html>
